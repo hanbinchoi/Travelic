1f79f4904f560865e5b87c8a12df553d75a25184
hanbinchoi <wkghskak@naver.com>
2021-09-29 21:39:08 +0900

    preload

--- js/continent.js
+++ js/continent.js
@@ -1,4 +1,4 @@
-window.onload = (() => {
+(() => {
     let i = 0;
     const txt = 'Welcome to Asia'; /* The text */
     const speed = 100; /* The speed/duration of the effect in milliseconds */
@@ -6,11 +6,19 @@ window.onload = (() => {
     const titleImge = document.querySelector(".img-container img");
 
     const imgSet = [
-        "../img/asia/0.jpg","../img/asia/1.jpg","../img/asia/2.jpg","../img/asia/3.jpg",
-        "../img/asia/4.jpg","../img/asia/5.jpg","../img/asia/6.jpg","../img/asia/7.jpg",
-        "../img/asia/8.jpg","../img/asia/9.jpg","../img/asia/10.jpg","../img/asia/11.jpg",
-        "../img/asia/12.jpg","../img/asia/13.jpg","../img/asia/14.jpg","../img/asia/15.jpg",
+        "../img/asia/0.jpg", "../img/asia/1.jpg", "../img/asia/2.jpg", "../img/asia/3.jpg",
+        "../img/asia/4.jpg", "../img/asia/5.jpg", "../img/asia/6.jpg", "../img/asia/7.jpg",
+        "../img/asia/8.jpg", "../img/asia/9.jpg", "../img/asia/10.jpg", "../img/asia/11.jpg",
+        "../img/asia/12.jpg", "../img/asia/13.jpg", "../img/asia/14.jpg", "../img/asia/15.jpg",
     ]
+
+    // image preload
+    function preload() {
+        for (i = 0; i < preload.arguments.length; i++) {
+            titleImge.src = preload.arguments[i];
+        }
+    }
+    preload(imgSet);
 
     function typeWriter() {
         if (i < txt.length) {
