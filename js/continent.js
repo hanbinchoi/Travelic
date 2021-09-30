1384cc4ec2c939502cad81d585c8f4ecd8630502
hanbinchoi <wkghskak@naver.com>
2021-09-30 21:50:55 +0900

    continent

--- js/continent.js
+++ js/continent.js
@@ -1,7 +1,35 @@
 (() => {
+
+    // sticky nav bar
+
+    const header = document.querySelector('header');
+    const headerHeight = header.getBoundingClientRect().height;
+    const leftNavbar = document.querySelector('.left-navbar');
+    document.addEventListener('scroll', () => {
+        if (matchMedia("screen and (max-width: 800px)").matches) {
+            // 800px 이상에서 사용할 JavaScript
+            leftNavbar.style.transform = "scaleY(0)";
+        } else {
+            // 800px 미만에서 사용할 JavaScript
+            if (window.scrollY > headerHeight + 100) {
+                // leftNavbar.style.display="inline-flex";
+                // leftNavbar.style.opacity = "1";
+                leftNavbar.style.transform = "scaleY(1)";
+            } else {
+                // leftNavbar.style.display="none"
+                // leftNavbar.style.opacity = "0";
+                leftNavbar.style.transform = "scaleY(0)";
+            }
+        }
+
+    })
+
+
+    // title animation
+
     let i = 0;
     const txt = 'Welcome to Asia'; /* The text */
-    const speed = 100; /* The speed/duration of the effect in milliseconds */
+    const speed = 120; /* The speed/duration of the effect in milliseconds */
 
     const titleImge = document.querySelector(".img-container img");
 
@@ -20,6 +48,19 @@
             titleImge.src = imgSet[i];
             setTimeout(typeWriter, speed);
         }
+        if (i === txt.length) {
+            titleImge.style.filter = "brightness(20%)";
+            titleImge.style.transition = "all 1000ms ease-in";
+        }
     }
-    var timeoutId = setTimeout(typeWriter,200);
+
+
+    typeWriter();
+    // toggle button event
+    const menu = document.querySelector('.countrys');
+    const toggle_btn = document.querySelector('.navbar_toggle-btn');
+    toggle_btn.addEventListener('click', () => {
+        menu.classList.toggle('open');
+    });
+
 })();
